57a93c82e14a301b3a578d6fe9a6ba5d889ce7c9
Ben Banister <desidiawvu@gmail.com>
2023-01-31 22:05:09 -0500

    quality of life and making it purtty

--- src/providers/webViewProvider.ts
+++ src/providers/webViewProvider.ts
@@ -20,18 +20,61 @@ export class WebViewProvider implements vscode.WebviewViewProvider {
                 (See the 'webview-sample' extension sample for img-src content security policy examples)
             -->
         </head>
+        <style nonce=${nonce}>
+            .vs-style {
+                background: var(--vscode-editor-background);
+                color: var(--vscode-editor-foreground);
+                font-family: var(--vscode-editor-font-family);
+                font-size: var(--vscode-editor-font-size);
+                font-weight: var(--vscode-editor-font-weight);
+            }
+            .button {
+                border-color: var(--vscode-editor-foreground);
+            }
+            .textarea {
+                border-color: var(--vscode-editor-foreground);
+                resize: none;
+                width: 100%;
+                height: 150px;
+                background: none;
+                margin: 10px 0px;
+            }
+        </style>
         <body>
-            <input id="input"/>
-            <button id="enter">enter</button>
-            <p id="answer"></p>
+            <div style="display: flex; flex-direction: column;">
+                <textarea class="vs-style textarea" id="input" class="vs-style"></textarea>
+                <div>
+                    <button class="button vs-style" id="enter">enter</button>
+                    <button class="button vs-style" id="clear">clear</button>
+                </div>
+                <p id="answer"></p>
+            </div>
             <script nonce=${nonce}>
                 const vscode = acquireVsCodeApi();
                 const input = document.getElementById('input');
                 document.getElementById('enter').addEventListener('click', (e) => {
                     vscode.postMessage({ type: 'newTextEntered', value: input.value }, '*');
                 });
+                document.getElementById('clear').addEventListener('click', (e) => {
+                    document.getElementById('answer').innerHTML = "";
+                    document.getElementById('input').value = "";
+                });
                 window.addEventListener('message', event => {
-                    document.getElementById('answer').innerHTML = event.data.answer;
+                    switch(event.data.type) {
+                        case 'message':
+                            document.getElementById('answer').innerHTML = event.data.value;
+                            break;
+                        case 'action':
+                            switch(event.data.value) {
+                                case 'disable-button':
+                                    document.getElementById('enter').disabled = true;
+                                    break;  
+                                case 'enable-button':
+                                    document.getElementById('enter').disabled = false;
+                                    break;
+                            }
+                            break;
+                    }
                 });
             </script>
         </body>
@@ -44,8 +87,16 @@ export class WebViewProvider implements vscode.WebviewViewProvider {
 			switch (data.type) {
 				case 'newTextEntered':
 					{
+                        if(!data.value) {
+                            webviewView.webview.postMessage({type: 'message', value: 'Please enter some text'});
+                            return;
+                        }
+                        webviewView.webview.postMessage({type: 'action', value: 'disable-button'});
+                        webviewView.webview.postMessage({type: 'message', value: 'Thinking...'});
                         getAnswer(data.value, 0, 4000).then(answer => {
-                            webviewView.webview.postMessage({ answer });
+                        webviewView.webview.postMessage({type: 'message', value: 'Thinking...'});
+                            webviewView.webview.postMessage({type: 'message', value: answer});
+                            webviewView.webview.postMessage({type: 'action', value: 'enable-button'});
                         });
 						break;
 					}
